3e2f1434d2b125f05cca549a036039034f7b37eb
Виталий Тамилин <vitaliktamilin@gmail.com>
2024-05-12 16:04:14 +0200

    add actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,6 +1,5 @@
-from pathlib import Path
-
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = False
 
 ALLOWED_HOSTS = [
-	'158.160.89.77', '127.0.0.1', 'localhost', 'sweeper-apps.ddns.net'
+    '158.160.89.77', '127.0.0.1', 'localhost', 'sweeper-apps.ddns.net'
 ]
 
 # Application definition
